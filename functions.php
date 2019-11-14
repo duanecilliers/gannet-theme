ae8ec6b813cc246f23338c834a582b4215501c17
Duane Cilliers <hello@duane.co.za>
2019-11-14 22:32:24 +0200

    Move css and js into assets directory

--- functions.php
+++ functions.php
@@ -147,9 +147,9 @@ add_action( 'widgets_init', 'gannet_widgets_init' );
 function gannet_scripts() {
 	wp_enqueue_style( 'gannet-style', get_template_directory_uri() . '/build/index.css' );
 
-	wp_enqueue_script( 'gannet-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
+	wp_enqueue_script( 'gannet-navigation', get_template_directory_uri() . '/assets/js/navigation.js', array(), '20151215', true );
 
-	wp_enqueue_script( 'gannet-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+	wp_enqueue_script( 'gannet-skip-link-focus-fix', get_template_directory_uri() . '/assets/js/skip-link-focus-fix.js', array(), '20151215', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
