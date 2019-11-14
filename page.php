73ead7c20bfd3f38a1f920d9c9d850f0f788fc3f
Duane Cilliers <hello@duane.co.za>
2019-11-14 22:25:56 +0200

    Refactor page template using twig

--- page.php
+++ page.php
@@ -12,29 +12,6 @@
  * @package Gannet
  */
 
-get_header();
-?>
-
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
-
-		<?php
-		while ( have_posts() ) :
-			the_post();
-
-			get_template_part( 'template-parts/content', 'page' );
-
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
-
-		endwhile; // End of the loop.
-		?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
-
-<?php
-get_sidebar();
-get_footer();
+$timber_post     = new Timber\Post();
+$context['post'] = $timber_post;
+Timber::render( [ 'page-' . $timber_post->post_name . '.twig', 'page.twig' ], $context );
