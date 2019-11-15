4f391524eeedb2ece20e60f3200804cb691410f9
Duane Cilliers <hello@duane.co.za>
2019-11-15 04:02:02 +0200

    Refactor archive archive template using Timber

--- archive.php
+++ archive.php
@@ -7,51 +7,10 @@
  * @package Gannet
  */
 
-get_header();
-?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
-
-		<?php if ( have_posts() ) : ?>
-			
-			<div class='flex mb-4'>
-
-				<header class="page-header">
-					<?php
-					the_archive_title( '<h1 class="page-title">', '</h1>' );
-					the_archive_description( '<div class="archive-description">', '</div>' );
-					?>
-				</header><!-- .page-header -->
-
-				<?php
-				/* Start the Loop */
-				while ( have_posts() ) :
-					the_post();
-
-					/*
-					* Include the Post-Type-specific template for the content.
-					* If you want to override this in a child theme, then include a file
-					* called content-___.php (where ___ is the Post Type name) and that will be used instead.
-					*/
-					get_template_part( 'template-parts/content', get_post_type() );
-
-				endwhile;
-
-				the_posts_navigation(); ?>
-
-			</div>
-
-		<?php else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif;
-		?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
-
-<?php
-get_sidebar();
-get_footer();
+$templates = array( 'archive.twig', 'index.twig' );
+$context = Timber::context();
+$context['title'] = get_the_archive_title();
+$context['description'] = get_the_archive_description();
+$context['posts'] = new Timber\PostQuery();
+Timber::render( $templates, $context );
