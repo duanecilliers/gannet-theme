f4a9e7e3eff68ef0ae8f9f7413cb0ba9c9d4771a
Duane Cilliers <hello@duane.co.za>
2019-11-15 04:10:15 +0200

    Refactor single template using Timber

--- single.php
+++ single.php
@@ -7,31 +7,16 @@
  * @package Gannet
  */
 
-get_header();
-?>
-
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
-
-		<?php
-		while ( have_posts() ) :
-			the_post();
-
-			get_template_part( 'template-parts/content', get_post_type() );
-
-			the_post_navigation();
-
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
-
-		endwhile; // End of the loop.
-		?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
-
-<?php
-get_sidebar();
-get_footer();
+$context         = Timber::context();
+$timber_post     = Timber::query_post();
+$context['post'] = $timber_post;
+if ( post_password_required( $timber_post->ID ) ) {
+	Timber::render( 'single-password.twig', $context );
+} else {
+	Timber::render( array(
+		'single-' . $timber_post->ID . '.twig',
+		'single-' . $timber_post->post_type . '.twig',
+		'single-' . $timber_post->slug . '.twig',
+		'single.twig'
+	), $context );
+}
