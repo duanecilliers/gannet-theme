fd1deb8a6878f728381212ee7dc2b7df1342f571
Duane Cilliers <hello@duane.co.za>
2019-11-16 12:52:42 +0200

    Update GannetPost class with method to fetch first video embed from post content

--- inc/timber/GannetPost.php
+++ inc/timber/GannetPost.php
@@ -2,12 +2,69 @@
 
 class GannetPost extends Timber\Post {
 
-    var $_format;
+    public $_format;
+
+    public $_first_video;
     
     public function format() {
         if ( !$this->_format ) {
             $this->_format = get_post_format( $this->ID ) ? : 'standard';
         }
         return $this->_format;
+    }
+
+    public function first_video() {
+        if ( !$this->_first_video ) {
+            global $shortcode_tags;
+        // Make a copy of global shortcode tags - we'll temporarily overwrite it.
+        $theme_shortcode_tags = $shortcode_tags;
+
+        // The shortcodes we're interested in.
+        $shortcode_tags = array(
+            'video' => $theme_shortcode_tags['video'],
+            'embed' => $theme_shortcode_tags['embed']
+        );
+        // Get the absurd shortcode regexp.
+        $video_regex = '#' . get_shortcode_regex() . '#i';
+
+        // Restore global shortcode tags.
+        $shortcode_tags = $theme_shortcode_tags;
+
+        $pattern_array = array( $video_regex );
+
+        // Get the patterns from the embed object.
+        if ( ! function_exists( '_wp_oembed_get_object' ) ) {
+            include ABSPATH . WPINC . '/class-oembed.php';
+        }
+        $oembed = _wp_oembed_get_object();
+        $pattern_array = array_merge( $pattern_array, array_keys( $oembed->providers ) );
+
+        // Or all the patterns together.
+        $pattern = '#(' . array_reduce( $pattern_array, function ( $carry, $item ) {
+            if ( strpos( $item, '#' ) === 0 ) {
+                // Assuming '#...#i' regexps.
+                $item = substr( $item, 1, -2 );
+            } else {
+                // Assuming glob patterns.
+                $item = str_replace( '*', '(.+)', $item );
+            }
+            return $carry ? $carry . ')|('  . $item : $item;
+        } ) . ')#is';
+
+        // Simplistic parse of content line by line.
+        $lines = explode( "\n", $this->post_content );
+        foreach ( $lines as $line ) {
+            $line = trim( $line );
+            if ( preg_match( $pattern, $line, $matches ) ) {
+                if ( strpos( $matches[0], '[' ) === 0 ) {
+                    $ret = do_shortcode( $matches[0] );
+                } else {
+                    $ret = wp_oembed_get( $matches[0] );
+                }
+                $this->_first_video = $ret;
+            }
+        }
+        }
+        return $this->_first_video;
     }
 }
